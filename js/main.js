a0654ce891e07a09fdc9eb88348eb990c256c78c
ppopow <pawel.popow@gmail.com>
2020-10-18 20:47:27 +0200

    nowe zadanie 33

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let text = document.getElementById('opis');
+let click = document.querySelector('button');
+
+button.onclick = function () {
+    opis(myHeading);
+}
+
+function opis (myHeading) {
+    myHeading.textContent = 'dowolny tekst';
+}
+
+
+
